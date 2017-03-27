b4c26c23b54e0d6d498cab46b356ec614e13a897
joongs <rlatpwnd8314@naver.com>
2017-03-28 06:59:19 +0900

    SE4 : 01 Date, C language - complete
    SE4 : 01 Date, C 언어 버전으로 구현

--- SE_Level4/DateCLanguage20170327/DateCLanguage20170327/Date.c
+++ SE_Level4/DateCLanguage20170327/DateCLanguage20170327/Date.c
@@ -34,7 +34,7 @@ void Create(Date *date) {
 
 /*
 함수 명칭 : Today
-기     능 : 날짜에서 오늘을 출력한다.
+기     능 : 오늘 날짜를 구하고 출력한다.
 입     력 : 없음
 출     력 : 오늘 날짜
 */
@@ -54,66 +54,158 @@ Date Today(Date *date) {
 }
 /*
 함수 명칭 : Yesterday
-기     능 : 어제 날짜를 출력한다.
+기     능 : 어제 날짜를 구하고, 출력한다.
 입     력 : 없음
 출     력 : 어제 날짜
 */
 Date Yesterday(Date *date) {
-
+	struct tm yesterday = { 0, };
+	Date yesterday_;
+	Create(&yesterday_);
+	yesterday.tm_year = date->year - 1900;
+	yesterday.tm_mon = date->month - 1;
+	yesterday.tm_mday = date->day - 1;
+	mktime(&yesterday);
+	yesterday_.year = yesterday.tm_year + 1900;
+	yesterday_.month = (Month)(yesterday.tm_mon + 1);
+	yesterday_.day = yesterday.tm_mday;
+	yesterday_.weekDay = yesterday.tm_mday;
+	Destroy(&yesterday_);
+	return yesterday_;
 }
 /*
-함수 명칭 :
-기     능 :
-입     력 :
-출     력 :
+함수 명칭 : Tommorrow
+기     능 : 내일 날짜를 구하고 출력한다.
+입     력 : 없음
+출     력 : 내일 날짜
 */
-Date Tommorrow(Date *date);
+Date Tommorrow(Date *date) {
+	struct tm tommorrow = { 0, };
+	Date tommorrow_;
+	Create(&tommorrow_);
+	tommorrow.tm_year = date->year - 1900;
+	tommorrow.tm_mon = date->month - 1;
+	tommorrow.tm_mday = date->day + 1;
+	mktime(&tommorrow);
+	tommorrow_.year = tommorrow.tm_year + 1900;
+	tommorrow_.month = (Month)(tommorrow.tm_mon + 1);
+	tommorrow_.day = tommorrow.tm_mday;
+	tommorrow_.weekDay = tommorrow.tm_mday;
+	Destroy(&tommorrow_);
+	return tommorrow_;
+}
 /*
-함수 명칭 :
-기     능 :
-입     력 :
-출     력 :
+함수 명칭 : PreviousDate
+기     능 : 이전의 날짜를 구하고 출력한다.
+입     력 : 없음
+출     력 : 이전 날짜
 */
-Date PreviousDate(Date *date);
+Date PreviousDate(Date *date, UShort days) {
+	struct tm previousDate = { 0, };
+	Date previousDate_;
+	Create(&previousDate_);
+	previousDate.tm_year = date->year - 1900;
+	previousDate.tm_mon = date->month - 1;
+	previousDate.tm_mday = date->day - days;
+	mktime(&previousDate);
+	previousDate_.year = previousDate.tm_year + 1900;
+	previousDate_.month = (Month)(previousDate.tm_mon + 1);
+	previousDate_.day = previousDate.tm_mday;
+	previousDate_.weekDay = previousDate.tm_mday;
+	Destroy(&previousDate_);
+	return previousDate_;
+}
 /*
-함수 명칭 :
-기     능 :
-입     력 :
-출     력 :
+함수 명칭 : NextDate
+기     능 : 다음의 날짜를 구하고, 출력한다.
+입     력 : 일수
+출     력 : 다음 날짜
 */
-Date NextDate(Date *date);
+Date NextDate(Date *date, UShort days) {
+	struct tm nextDate = { 0, };
+	Date nextDate_;
+	Create(&nextDate_);
+	nextDate.tm_year = date->year - 1900;
+	nextDate.tm_mon = date->month - 1;
+	nextDate.tm_mday = date->day + days;
+	mktime(&nextDate);
+	nextDate_.year = nextDate.tm_year + 1900;
+	nextDate_.month = (Month)(nextDate.tm_mon + 1);
+	nextDate_.day = nextDate.tm_mday;
+	nextDate_.weekDay = nextDate.tm_mday;
+	Destroy(&nextDate_);
+	return nextDate_;
+}
 /*
-함수 명칭 :
-기     능 :
-입     력 :
-출     력 :
+함수 명칭 : IsEqual
+기     능 : 입력받은 날짜들이 같은지 확인하고 비교결과를 출력한다.
+입     력 : 날짜
+출     력 : 비교결과
 */
-Boolean IsEqual(Date *date, Date *other);
+Boolean IsEqual(Date *date, Date *other) {
+	Boolean ret = FALSE;
+	if (date->year == other->year && date->month == other->month && date->day == other->day) {
+		ret = TRUE;
+	}
+	return ret;
+}
 /*
-함수 명칭 :
-기     능 :
-입     력 :
-출     력 :
+함수 명칭 : IsNotEqual
+기     능 : 입력받은 날짜들이 같지않은지 확인하고 비교결과를 출력한다.
+입     력 : 날짜
+출     력 : 비교결과
 */
-Boolean IsNotEqual(Date *date, Date *other);
+Boolean IsNotEqual(Date *date, Date *other) {
+	Boolean ret = FALSE;
+	if (date->year != other->year || date->month != other->month || date->day != other->day) {
+		ret = TRUE;
+	}
+	return ret;
+}
 /*
-함수 명칭 :
-기     능 :
-입     력 :
-출     력 :
+함수 명칭 : IsGreaterThen
+기     능 : 입력받은 날짜보다 더 큰 날짜인지 확인하고, 결과를 출력한다.
+입     력 : 날짜
+출     력 : 비교결과
 */
-Boolean IsGreaterThen(Date *date, Date *other);
+Boolean IsGreaterThen(Date *date, Date *other) {
+	Boolean ret = FALSE;
+	if (date->year > other->year) {
+		ret = TRUE;
+	}
+	else if (date->year == other->year && date->month > other->month) {
+		ret = TRUE;
+	}
+	else if (date->year == other->year && date->month == other->month && date->day > other->day) {
+		ret = TRUE;
+	}
+	return ret;
+}
 /*
-함수 명칭 :
-기     능 :
-입     력 :
-출     력 :
+함수 명칭 : IsLesserThen
+기     능 : 입력받은 날짜보다 더 작은 날짜인지 확인하고, 결과를 출력한다.
+입     력 : 날짜
+출     력 : 비교결과
 */
-Boolean IsLesserThen(Date *date, Date *other);
+Boolean IsLesserThen(Date *date, Date *other) {
+	Boolean ret = FALSE;
+	if (date->year < other->year) {
+		ret = TRUE;
+	}
+	else if (date->year == other->year && date->month < other->month) {
+		ret = TRUE;
+	}
+	else if (date->year == other->year && date->month == other->month && date->day < other->day) {
+		ret = TRUE;
+	}
+	return ret;
+}
 /*
-함수 명칭 :
-기     능 :
-입     력 :
-출     력 :
+함수 명칭 : Destroy
+기     능 : 날짜를 해제한다.
+입     력 : 없음
+출     력 : 없음
 */
-void Destroy(Date *date);
+void Destroy(Date *date) {
+
+}
---
+++ SE_Level4/DateCLanguage20170327/DateCLanguage20170327/Main.c
@@ -0,0 +1,144 @@
+//Main.c
+/*
+파일 명칭 : Main.c
+기     능 : 날짜 기능을 제공한다.
+작 성 자 : Joey
+작성 일자 : 2017/03/27
+*/
+
+#include "Date.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	Date date;
+	Date today;
+	Date yesterday;
+	Date tommorrow;
+	Date previousDate;
+	Date nextDate;
+	Boolean ret;
+
+	//생성한다.
+	Create(&date);
+	Create(&today);
+	Create(&yesterday);
+	Create(&tommorrow);
+	Create(&previousDate);
+	Create(&nextDate);
+
+	//오늘
+	today = date.Today(&date);
+	printf("%4d-%02d-%02d-", today.year, today.month, today.day);
+	switch(today.weekDay) {
+	case SUNDAY: printf("일요일\n"); break;
+		case MONDAY: printf("월요일\n"); break;
+		case TUESDAY: printf("화요일\n"); break;
+		case WEDNESDAY: printf("수요일\n"); break;
+		case THURSDAY: printf("목요일\n"); break;
+		case FRIDAY: printf("금요일\n"); break;
+		case SATURDAY: printf("토요일\n"); break;
+		defalut: break;
+	}
+
+	//어제
+	yesterday = date.Yesterday(&today);
+	printf("%4d-%02d-%02d-", yesterday.year, yesterday.month, yesterday.day);
+	switch (today.weekDay) {
+		case SUNDAY: printf("일요일\n"); break;
+		case MONDAY: printf("월요일\n"); break;
+		case TUESDAY: printf("화요일\n"); break;
+		case WEDNESDAY: printf("수요일\n"); break;
+		case THURSDAY: printf("목요일\n"); break;
+		case FRIDAY: printf("금요일\n"); break;
+		case SATURDAY: printf("토요일\n"); break;
+		default: break;
+	}
+
+	//내일
+	tommorrow = date.Tommorrow(&yesterday);
+	printf("%4d-%02d-%02d-", tommorrow.year, tommorrow.month, tommorrow.day);
+	switch (today.weekDay) {
+		case SUNDAY: printf("일요일\n"); break;
+		case MONDAY: printf("월요일\n"); break;
+		case TUESDAY: printf("화요일\n"); break;
+		case WEDNESDAY: printf("수요일\n"); break;
+		case THURSDAY: printf("목요일\n"); break;
+		case FRIDAY: printf("금요일\n"); break;
+		case SATURDAY: printf("토요일\n"); break;
+		default: break;
+	}
+
+	//이전날짜
+	previousDate = date.PreviousDate(&today, 3);
+	printf("%4d-%02d-%02d-", previousDate.year, previousDate.month, previousDate.day);
+	switch (today.weekDay) {
+		case SUNDAY: printf("일요일\n"); break;
+		case MONDAY: printf("월요일\n"); break;
+		case TUESDAY: printf("화요일\n"); break;
+		case WEDNESDAY: printf("수요일\n"); break;
+		case THURSDAY: printf("목요일\n"); break;
+		case FRIDAY: printf("금요일\n"); break;
+		case SATURDAY: printf("토요일\n"); break;
+		default: break;
+	}
+	
+	//이후날짜
+	nextDate = date.NextDate(&today, 4);
+	printf("%4d-%02d-%02d-", nextDate.year, nextDate.month, nextDate.day);
+	switch (today.weekDay) {
+		case SUNDAY: printf("일요일\n"); break;
+		case MONDAY: printf("월요일\n"); break;
+		case TUESDAY: printf("화요일\n"); break;
+		case WEDNESDAY: printf("수요일\n"); break;
+		case THURSDAY: printf("목요일\n"); break;
+		case FRIDAY: printf("금요일\n"); break;
+		case SATURDAY: printf("토요일\n"); break;
+		default: break;
+	}
+
+	//같은지
+	ret = tommorrow.IsEqual(&tommorrow, &today);
+	if (ret == TRUE) {
+		printf("같습니다.\n");
+	}
+	else {
+		printf("같지 않습니다.\n");
+	}
+
+	//다른지
+	ret = yesterday.IsNotEqual(&yesterday, &today);
+	if (ret == TRUE) {
+		printf("같습니다.\n");
+	}
+	else {
+		printf("같지 않습니다.\n");
+	}
+
+	//더큰지
+	ret = today.IsGreaterThen(&today, &yesterday);
+	if (ret == TRUE) {
+		printf("같습니다.\n");
+	}
+	else {
+		printf("같지 않습니다.\n");
+	}
+
+	//더작은지
+	ret = previousDate.IsLesserThen(&previousDate, &nextDate);
+	if (ret == TRUE) {
+		printf("같습니다.\n");
+	}
+	else {
+		printf("같지 않습니다.\n");
+	}
+
+	//지운다.
+	Destroy(&date);
+	Destroy(&today);
+	Destroy(&yesterday);
+	Destroy(&tommorrow);
+	Destroy(&previousDate);
+	Destroy(&nextDate);
+
+	return 0;
+}
