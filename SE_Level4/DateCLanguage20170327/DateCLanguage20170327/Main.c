64c2d0bc3b6da6fade8859dc10b615dbfbc84c72
joongs <rlatpwnd8314@naver.com>
2017-04-02 11:02:18 +0900

    문접오류 수정, 출력문 변경

--- SE_Level4/DateCLanguage20170327/DateCLanguage20170327/Main.c
+++ SE_Level4/DateCLanguage20170327/DateCLanguage20170327/Main.c
@@ -37,7 +37,7 @@ int main(int argc, char *argv[]) {
 		case THURSDAY: printf("목요일\n"); break;
 		case FRIDAY: printf("금요일\n"); break;
 		case SATURDAY: printf("토요일\n"); break;
-		defalut: break;
+		default: break;
 	}
 
 	//어제
@@ -108,28 +108,28 @@ int main(int argc, char *argv[]) {
 	//다른지
 	ret = yesterday.IsNotEqual(&yesterday, &today);
 	if (ret == TRUE) {
-		printf("같습니다.\n");
+		printf("다릅니다.\n");
 	}
 	else {
-		printf("같지 않습니다.\n");
+		printf("다르지 않습니다.\n");
 	}
 
 	//더큰지
 	ret = today.IsGreaterThen(&today, &yesterday);
 	if (ret == TRUE) {
-		printf("같습니다.\n");
+		printf("더 나중입니다.\n");
 	}
 	else {
-		printf("같지 않습니다.\n");
+		printf("더 나중이 아닙니다.\n");
 	}
 
 	//더작은지
 	ret = previousDate.IsLesserThen(&previousDate, &nextDate);
 	if (ret == TRUE) {
-		printf("같습니다.\n");
+		printf("더 이전입니다.\n");
 	}
 	else {
-		printf("같지 않습니다.\n");
+		printf("더 이전이 아닙니다.\n");
 	}
 
 	//지운다.
